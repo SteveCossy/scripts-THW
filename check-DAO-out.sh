4627f39fdc291b2f084dd75fcef046f284bea1c7
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-08-25 18:20:05 +1200

    THW-2025-08-25_18:20:00

--- check-DAO-out.sh
+++ check-DAO-out.sh
@@ -1,11 +1,19 @@
 # Checking contents of log files
 
-echo -n "fd00 lines: "
-grep -n "DAO lifetime:" $1 | grep "prefix: fd00" |  wc -l 
-echo -n "fd02 lines: "
-grep -n "DAO lifetime:" $1 | grep "prefix: fd02" |  wc -l
-echo -n "fd00 lines also with fd02: "
-grep -n "DAO lifetime:" $1 | grep "prefix: fd00" | grep fd02 | wc -l
-echo -n "fd02 lines also with fd00: "
-grep -n "DAO lifetime:" $1 | grep "prefix: fd02" | grep fd00 | wc -l
+#SEARCH="Sending a DAO with"
+SEARCH="DAO lifetime:"
+TEST1a="prefix: fd00"
+TEST1b="prefix: fd02"
+
+TEST2a="fd00"
+TEST2b="fd02"
+
+echo -n "$SEARCH with $TEST1a lines: "
+grep -n "$SEARCH" $1 | grep "$TEST1a" |  wc -l 
+echo -n "$SEARCH with $TEST1b lines: "
+grep -n "$SEARCH" $1 | grep "$TEST1b" |  wc -l
+echo -n "$SEARCH with $TEST1a & not $TEST2a lines: "
+grep -n "$SEARCH" $1 | grep "$TEST1a" | grep $TEST2b | wc -l
+echo -n "$SEARCH with $TEST1b & not $TEST2a lines: "
+grep -n "$SEARCH" $1 | grep "$TEST1b" | grep $TEST2a | wc -l
 
