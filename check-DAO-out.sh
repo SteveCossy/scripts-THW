133bb11826dc96e99010f93219ec7a4d096baaa6
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-08-26 13:40:05 +1200

    THW-2025-08-26_13:40:01

--- check-DAO-out.sh
+++ check-DAO-out.sh
@@ -1,17 +1,20 @@
 # Checking contents of log files
 
+FUNCTION="dao_input_nonstoring()"
 SEARCH="Sending a DAO with"
-TEST1a="prefix: fd00"
-TEST1b="prefix: fd02"
+TEST1a="fd00"
+TEST1b="fd02"
 TEST2a="fd00"
 TEST2b="fd02"
 
+#FUNCTION="dao_input_nonstoring()"
 #SEARCH="DAO lifetime:"
 #TEST1a="prefix: fd00"
 #TEST1b="prefix: fd02"
 #TEST2a="fd00"
 #TEST2b="fd02"
 
+echo Parsing output from $FUNCTION
 echo -n "$SEARCH with $TEST1a lines: "
 grep -n "$SEARCH" $1 | grep "$TEST1a" |  wc -l 
 echo -n "$SEARCH with $TEST1b lines: "
