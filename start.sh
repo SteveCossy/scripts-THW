c7bc6679cb24b57ceb1a90554570fc98f5c14808
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-10-26 20:40:04 +1300

    THW-2025-10-26_20:40:00

---
+++ start.sh
@@ -0,0 +1,2 @@
+vncviewer regent:22 > /dev/null 2>&1  &
+okular /home/stevecos/Documents/techstillmore/Contiki-NG-Flowcharts2.pdf /home/stevecos/Documents/techstillmore/occurences+refactoring.pdf /home/stevecos/Documents/techstillmore/Contiki-NG-Flowcharts.pdf /home/stevecos/Documents/techstillmore/Contiki-NG_TwoDODAG.pdf /home/stevecos/data/SimSummary.pdf  > /dev/null 2>&1  &
