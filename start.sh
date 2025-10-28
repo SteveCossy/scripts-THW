2ad8dec8322cdb689f463864f285fce0bcfeeb2b
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-10-29 09:40:04 +1300

    THW-2025-10-29_09:40:00

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
-vncviewer regent:22 > /dev/null 2>&1  &
-okular /home/stevecos/Documents/techstillmore/Contiki-NG-Flowcharts2.pdf /home/stevecos/Documents/techstillmore/occurences+refactoring.pdf /home/stevecos/Documents/techstillmore/Contiki-NG-Flowcharts.pdf /home/stevecos/Documents/techstillmore/Contiki-NG_TwoDODAG.pdf /home/stevecos/data/SimSummary.pdf  > /dev/null 2>&1  &
+# Check that machine name matches parameter supplied.
+# Check whether required process is running. Start if not
+# 29 October 2025
+
+if [ "$HOSTNAME" ="$1.ecs.vuw.ac.nz" ] ; then
+    PROCESS_NAME="vncviewer"
+    COMMAND_TO_RUN="echo $1 not running" # Replace with the command to execute
+    if ! pgrep -x "$PROCESS_NAME" > /dev/null; then
+        echo "$PROCESS_NAME is not running. Starting startup systems now..."
+        vncviewer regent:22 > /dev/null 2>&1  &
+        okular /home/stevecos/Documents/techstillmore/Contiki-NG-Flowcharts2.pdf /home/stevecos/Documents/techstillmore/occurences+refactoring.pdf /home/stevecos/Documents/techstillmore/Contiki-NG-Flowcharts.pdf /home/stevecos/Documents/techstillmore/Contiki-NG_TwoDODAG.pdf /home/stevecos/data/SimSummary.pdf  > /dev/null 2>&1  &
+    else
+        echo "$PROCESS_NAME is already running."
+    fi
+else
+    echo "Not logging into $1 so not running startup"
+fi
